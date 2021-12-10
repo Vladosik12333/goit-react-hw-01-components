27b6db0e9b92a8baf11ce22d00475bb9663e2f81
Vlad <vvllaadd1233345@gmail.com>
2021-12-11 01:09:16 +0200

    1,2 taskts without styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Fragment } from "react";
-import Button from "components/button";
-import base from "./base.json";
+import User from "components/Task-1";
+import Statistics from "components/Task-2";
+import baseUser from "./base/user.json";
+import baseStatic from "./base/data.json";
 
+const { username, tag, location, avatar, stats } = baseUser;
 export default function App() {
   return (
     <Fragment>
-      <Button array={base} />
+      <User
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+      <Statistics title="First" stats={baseStatic} />
+      <Statistics stats={baseStatic} />
     </Fragment>
   );
 }
---
+++ src/components/Task-2/Statistics.js
@@ -0,0 +1,25 @@
+import propTypes from "prop-types";
+import styles from "./Statistics.module.css";
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li key={id} className="item">
+              <span className="label">{label} </span>
+              <span className="percentage">{percentage} </span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: propTypes.string,
+  stats: propTypes.array,
+};
